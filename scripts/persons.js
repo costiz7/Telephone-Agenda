56456fefd43cc84742802ee32b0f913e8f5236f3
Zanescu Costel <costelzanescu7@gmail.com>
2025-10-28 00:20:34 +0200

    Added Error Handlers

--- scripts/persons.js
+++ scripts/persons.js
@@ -11,7 +11,7 @@ class Person {
 }
 
 //This is going to be the object array for every 'Person' object
-const personArray = [];
+window.personArray = [];
 
 //This Event Listener adds a 'Person' object from the input to the array and then displays it on the page
 document.getElementById('btn-add').addEventListener('click', () => {
@@ -19,9 +19,19 @@ document.getElementById('btn-add').addEventListener('click', () => {
     const firstName = document.getElementById('firstName').value;
     const phoneNumber = document.getElementById('phoneNumber').value;
 
+    if(checkAddErrors(lastName, firstName, phoneNumber)) return;
+    
     personArray.push(new Person(lastName, firstName, phoneNumber));
 
     addPerson(personArray);
+    if(Person.count > 0)
+        document.getElementById('table-wrapper').classList.add('opacity');
+    document.getElementById('lastName').placeholder="";
+    document.getElementById('lastName').value = "";
+    document.getElementById('firstName').placeholder="";
+    document.getElementById('firstName').value = "";
+    document.getElementById('phoneNumber').placeholder="";
+    document.getElementById('phoneNumber').value = "";
 });
 
 //This Event Listener removes a 'Person' object from the input in the array and then displays the remaining objects on the page
@@ -36,15 +46,22 @@ document.getElementById('btn-delete').addEventListener('click', () => {
         person.phoneNumber === phoneNumber
     );
 
-    if (index !== -1) {
+    if (index !== -1 || !checkDeleteErrors(lastName, firstName, phoneNumber)) {
         personArray.splice(index, 1);
-        console.log('Persoană ștearsă:', lastName, firstName);
         Person.count--;
     } else {
-        console.log('Persoana nu a fost găsită');
+        return;
     }
 
     addPerson(personArray);
+    if(Person.count <= 0)
+        document.getElementById('table-wrapper').classList.remove('opacity');
+    document.getElementById('lastName').placeholder="";
+    document.getElementById('lastName').value = "";
+    document.getElementById('firstName').placeholder="";
+    document.getElementById('firstName').value = "";
+    document.getElementById('phoneNumber').placeholder="";
+    document.getElementById('phoneNumber').value = "";;
 });
 
 //This function displays all the 'Person' objects on the page
@@ -66,4 +83,81 @@ function addPerson(personArray) {
                 <td>${person.phoneNumber}</td>
             </tr>`;
     } )
+    document.querySelector('#table-wrapper > span').textContent = `Number of people: `;
+    document.querySelector('#table-wrapper > span').textContent += `${Person.count}`;
+}
+
+function checkAddErrors(lastName, firstName, phoneNumber){
+    let checkError = false;
+    if(!lastName){
+        const wrapper = document.getElementById('lastName').closest('.input-wrapper');
+        wrapper.classList.add('shake');
+        document.getElementById('lastName').placeholder="No Last Name? :(";
+
+        setTimeout(() => {
+            wrapper.classList.remove('shake');
+        }, 300);
+        checkError = true;
+    }
+    if(!firstName){
+        const wrapper = document.getElementById('firstName').closest('.input-wrapper');
+        wrapper.classList.add('shake');
+        document.getElementById('firstName').placeholder="No First Name? :(";
+
+        setTimeout(() => {
+            wrapper.classList.remove('shake');
+        }, 300);
+        checkError = true;
+    }
+    if(!phoneNumber){
+        const wrapper = document.getElementById('phoneNumber').closest('.input-wrapper');
+        wrapper.classList.add('shake');
+        document.getElementById('phoneNumber').placeholder="No phone number? </3";
+
+        setTimeout(() => {
+            wrapper.classList.remove('shake');
+        }, 300);
+        checkError = true;
+    }
+
+    return checkError;
+}
+
+function checkDeleteErrors(lastName, firstName, phoneNumber){
+    let checkError = false;
+    if(!lastName || !personArray.some(p => p.lastName === lastName)){
+        const wrapper = document.getElementById('lastName').closest('.input-wrapper');
+        wrapper.classList.add('shake');
+        document.getElementById('lastName').value="";
+        document.getElementById('lastName').placeholder="This doesn't exist! ._.";
+
+        setTimeout(() => {
+            wrapper.classList.remove('shake');
+        }, 300);
+        checkError = true;
+    }
+    if(!firstName || !personArray.some(p => p.firstName === firstName)){
+        const wrapper = document.getElementById('firstName').closest('.input-wrapper');
+        wrapper.classList.add('shake');
+        document.getElementById('firstName').value="";
+        document.getElementById('firstName').placeholder="This doesn't exist! ._.";
+
+        setTimeout(() => {
+            wrapper.classList.remove('shake');
+        }, 300);
+        checkError = true;
+    }
+    if(!phoneNumber || !personArray.some(p => p.phoneNumber === phoneNumber)){
+        const wrapper = document.getElementById('phoneNumber').closest('.input-wrapper');
+        wrapper.classList.add('shake');
+        document.getElementById('phoneNumber').value="";
+        document.getElementById('phoneNumber').placeholder="This doesn't exist! ._.";
+
+        setTimeout(() => {
+            wrapper.classList.remove('shake');
+        }, 300);
+        checkError = true;
+    }
+
+    return checkError;
 }
