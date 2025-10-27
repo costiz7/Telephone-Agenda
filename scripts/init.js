6b6aef4f7ee630e7e4b601b455d5404004e985b0
Zanescu Costel <costelzanescu7@gmail.com>
2025-10-28 00:31:01 +0200

    Testing DarkTheme

--- scripts/init.js
+++ scripts/init.js
@@ -1,1 +1,6 @@
 //This script was made for implementing cache functionality
+const toggleBtn = document.getElementById('toggle');
+
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme');
+});
