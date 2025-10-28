76244c25067248f8a050e4535472a3de770de4b0
Zanescu Costel <costelzanescu7@gmail.com>
2025-10-28 14:54:15 +0200

    It is kinda finished and hosted on Vercel

--- scripts/init.js
+++ scripts/init.js
@@ -1,6 +1,7 @@
 //This script was made for implementing cache functionality
-const toggleBtn = document.getElementById('toggle');
+const toggleBtn = document.querySelector('.flip-button');
 
 toggleBtn.addEventListener('click', () => {
   document.body.classList.toggle('dark-theme');
+  document.querySelector('.flip-button').classList.toggle('flipped');
 });
